8b25e14b5e721bbeed5140890aaddf6e5b2ac703
Jake Ayers <jcayers20@gmail.com>
2021-06-30 13:49:34 -0500

    finish R solution for problem 3

--- src/problem_3/r/problem_3_solution.R
+++ src/problem_3/r/problem_3_solution.R
@@ -1,11 +1,28 @@
-get_primes <- function(upper_lim) {
-  primes <- c(2)
-
-  for(i in 3:upper_lim) {
-    if(all(i %% primes != 0)) {
-      primes <- append(primes, i)
+# get smallest prime factor of a given integer
+smallest_prime_factor <- function(n) {
+  
+  for(i in 2:n) {
+    
+    # first number evenly divisible by n will be smallest prime factor
+    if(n %% i == 0) {
+      return(i)
     }
   }
-
-  return(primes)
 }
+
+# get prime factorization of a number
+prime_factorization <- function(n) {
+  factors <- c()
+  quotient <- n
+  
+  while(quotient > 1) {
+    fac <- smallest_prime_factor(quotient)
+    factors <- append(factors, fac)
+    quotient <- quotient / fac
+  }
+  
+  return(factors)
+}
+
+factorization <- prime_factorization(600851475143)
+print(max(factorization))
